5f63f389ac0837fa32eee0a747827db547cc131f
jokedst <jokedst@gmail.com>
2014-09-16 16:27:01 +0200

    Should work on iPhones now. Maybe.

--- glyphs.js
+++ glyphs.js
@@ -238,3 +238,48 @@ var targ = [{ x: 50, y: 14.4337567 },
     { x: 12.5, y: 79.385662 },
     { x: 87.5, y: 79.385662 },
     { x: 50, y: 101.0362971 }];
+
+function NoClickDelay(el) {
+	this.element = el;
+	// this is a safari hack. I know it's stupid and fragile.
+	if( window.Touch && navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0){ 
+		this.element.addEventListener('touchstart', this, false);
+	}
+}
+
+// Stops 300 ms click delay on iOS/Safari
+NoClickDelay.prototype = {
+	handleEvent: function(e) {
+		switch(e.type) {
+			case 'touchstart': this.onTouchStart(e); break;
+			case 'touchmove': this.onTouchMove(e); break;
+			case 'touchend': this.onTouchEnd(e); break;
+		}
+	},
+
+	onTouchStart: function(e) {
+		e.preventDefault();
+		this.moved = false;
+		this.element.addEventListener('touchmove', this, false);
+		this.element.addEventListener('touchend', this, false);
+	},
+
+	onTouchMove: function(e) {
+		this.moved = true;
+	},
+
+	onTouchEnd: function(e) {
+		this.element.removeEventListener('touchmove', this, false);
+		this.element.removeEventListener('touchend', this, false);
+
+		if( !this.moved ) {
+			// Place your code here or use the click simulation below
+			var theTarget = document.elementFromPoint(e.changedTouches[0].clientX, e.changedTouches[0].clientY);
+			if(theTarget.nodeType == 3) theTarget = theTarget.parentNode;
+
+			var theEvent = document.createEvent('MouseEvents');
+			theEvent.initEvent('click', true, true);
+			theTarget.dispatchEvent(theEvent);
+		}
+	}
+};
