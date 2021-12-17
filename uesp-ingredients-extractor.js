51419fb6a73bbd1177b35d44f2b90b326e6783d9
Joakim Wennergren <joakim.wennergren@neovici.se>
2021-12-17 18:14:45 +0100

    New lists of ingredients and effects

--- uesp-ingredients-extractor.js
+++ uesp-ingredients-extractor.js
@@ -1,3 +1,5 @@
+// Attempt to extract stuff from https://en.uesp.net/wiki/Skyrim:Ingredients
+
 {
   "name": "Abecean Longfin",
   "cost": 44,
@@ -42,7 +44,7 @@ function row2obj(tr){
 }
 row2obj(t)
 
-function row2obj(tr){
+function row2obj(tr, group){
 	let ret = {};
 	ret.name = tr.children[1].firstElementChild.innerText;
 	ret.code = tr.children[1].lastElementChild.innerText;
@@ -50,28 +52,50 @@ function row2obj(tr){
 	ret.value = tr.nextElementSibling.children[4].innerText * 1;
 	ret.weight = tr.nextElementSibling.children[5].innerText * 1;
 	ret.merchants = tr.nextElementSibling.children[6].innerText || 'None';
-	
+	let dlc = tr.children[1].querySelector('sup>ba')?.title.replace(/^(Skyrim:)/,"");
+	if(dlc) ret.groups = [dlc];
+	else if(group) ret.groups = [group];
+
 	ret.effects = [];
 	let effects = [...tr.nextElementSibling.children].slice(0,4);
 	for(let i=0;i<4;i++){
 		let effect = effects[i];
-		if(effect.innerText.indexOf('(') == -1)
-			ret.effects.push(effect.innerText);
-		else{
+		let effectText = effect.firstElementChild.title;
+		if(effect.innerText.indexOf('(') != -1){
 			let mods = [...effect.querySelectorAll('span')];
 			for(let m=0;m<mods.length;m++){
 				let mod = mods[m];
-				switch(mod.nextElementSibling.title)
+				let value = mod.firstElementChild.innerText * 1;
+				// Sometimes the image follows the span, sometimes it's INSIDE the span. Seems like a bug in UESP
+				let img = mod.nextElementSibling?.tagName == 'A' ? mod.nextElementSibling : mod.querySelector('a');
+				switch(img.title){
+					case 'Value': effectText += ' (' + value + '€)'; break;
+					case 'Duration': effectText += ' (' + value + 'dur)'; break;
+					case 'Magnitude': effectText += ' (' + value + '×)'; break;
+				}
 			}
 		}
+		ret.effects.push(effectText);
 	}
-	
-	ret.effects = [...tr.nextElementSibling.children].slice(0,4).map(x=>x.innerText.trim());
 	return ret;
 }
+// 6+73+109 = 188
+function table2arr(table, group){
+	let rows = [...table.querySelectorAll('td[style="width:1px; padding:1px"]')].map(x=>x.parentElement);
+	let o = rows.map(tr => row2obj(tr, group));
+	return o;
+}
 
+function allTables(){
+	let tables = document.querySelectorAll('table.wikitable');
+	let arr = table2arr(tables[0]);
+	arr.push(...table2arr(tables[1], 'Creation Club'));
+	arr.push(...table2arr(tables[2], 'Unique/Quest'));
+	arr.sort((a,b) => a.name.localeCompare(b.name));
+	return arr;
+}
 
-
+document.querySelectorAll('td[style="width:1px; padding:1px"]')
  [...tbody.querySelectorAll('td[style="width:1px; padding:1px"]')].map(x=>x.parentElement)
  
  row2obj([...tbody.querySelectorAll('td[style="width:1px; padding:1px"]')].map(x=>x.parentElement)[1])
@@ -100,3 +124,48 @@ row2obj(t)
 	effects: [...tr.nextElementSibling.children].slice(0,4).map(x=>x.innerText.trim())
 	}))
 	
+
+// =========================================
+// Effects
+// =========================================
+
+// tr's
+document.querySelector('table.sortable').tBodies[0].children
+
+function parseEffects(){
+	// "Damage Magicka Regen":{isPotion:false,fixedMagnitude:true,baseMagnitude:100,fixedDuration:false,baseDuration:5,description:"Decrease the target's Magicka regeneration by <mag>% for <dur> seconds.",baseGold:0.5}
+	let rows = document.querySelector('table.sortable').tBodies[0].children;
+	let effects = {}
+	for(let i=0;i<rows.length;i++){
+		let tr = rows[i];
+		let name = tr.querySelector('th a').innerText;
+		effects[name] = {
+			"isPotion": tr.firstElementChild.className == 'EffectPos',
+			"fixedMagnitude": tr.children[4].className == 'EffectNeg',
+			"baseMagnitude": tr.children[4].innerText * 1,
+			"fixedDuration": tr.children[5].className != 'EffectPos',
+			"baseDuration": tr.children[5].innerText * 1,
+			"description": tr.children[2].innerText,
+			"baseGold": tr.children[3].innerText * 1
+		  };
+		  // Some values are missing on UESP
+		  if(effects[name].baseMagnitude == null || effects[name].baseDuration == null || effects[name].baseGold == null) effects[name].wildGuess = true;
+		  if(effects[name].baseMagnitude == null) effects[name].baseMagnitude = 1;
+		  if(effects[name].baseDuration == null) effects[name].baseDuration = 30;
+		  if(effects[name].baseGold == null) effects[name].baseGold = 0.1;
+	}
+	return effects;
+}
+
+function parserow(tr){
+	let ret = {
+		"isPotion": tr.firstElementChild.className == 'EffectPos',
+		"fixedMagnitude": tr.children[4].className == 'EffectNeg',
+		"baseMagnitude": tr.children[4].innerText * 1,
+		"fixedDuration": tr.children[5].className != 'EffectPos',
+		"baseDuration": tr.children[5].innerText * 1,
+		"description": tr.children[2].innerText,
+		"baseGold": tr.children[3].innerText * 1
+	  };
+	  return ret;
+}
